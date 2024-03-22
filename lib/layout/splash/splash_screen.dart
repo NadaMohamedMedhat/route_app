9fb3a34c464415edbbf0ec60f3f2cffbf9d87992
Nada Elshahawi <76527313+NadaMohamedMedhat@users.noreply.github.com>
2024-03-23 01:43:36 +0200

    - handle splash_screen.dart

--- lib/layout/splash/splash_screen.dart
+++ lib/layout/splash/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
+import '../../shared/providers/theme_provider.dart';
 import '../../shared/remote/firebase_auth/auth_provider.dart';
 import '../auth/login/login_screen.dart';
 import '../home/home_screen.dart';
@@ -17,13 +18,16 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    ThemeMode themeMode = Provider.of<ThemeProvider>(context).themeMode;
     Future.delayed(const Duration(seconds: 2), () {
       checkAutoLogin();
     });
     return Scaffold(
       body: Center(
         child: SvgPicture.asset(
-          'assets/images/route.svg',
+          themeMode == ThemeMode.light
+              ? 'assets/images/route.svg'
+              : 'assets/images/route_dark.svg',
         ),
       ),
     );
